0497811ef6b451e39c8c8da6c32e7264327e2adf
marArcz <mar19arcz@gmail.com>
2023-09-09 10:28:58 +0800

    added command schedules

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Console;
 
 use App\Console\Commands\DueSubmissions;
+use App\Console\Commands\NotificationForEventsToday;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -14,7 +15,8 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command(DueSubmissions::class)->daily();
+        $schedule->command(DueSubmissions::class)->daily()->withoutOverlapping();
+        $schedule->command(NotificationForEventsToday::class)->daily()->withoutOverlapping();
     }
 
     /**
